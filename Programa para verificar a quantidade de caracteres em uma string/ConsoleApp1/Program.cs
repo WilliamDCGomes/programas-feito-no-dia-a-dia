9bec55bcef300f549fbead8194da7775aaeeb2c2
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-27 12:49:56 -0300

    Programa para verificar a quantidade de caracteres em uma string

---
+++ Programa para verificar a quantidade de caracteres em uma string/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Word> Letters = new List<Word>();
+            List<char> LetterComp = new List<char>();
+            Console.WriteLine("Tap the name that you want");
+            string name = Console.ReadLine().ToUpper();
+            for(int i = 0; i < name.Length; i++)
+            {
+                if (LetterComp.Contains(name[i]))
+                {
+                    foreach (Word letter in Letters)
+                    {
+                        if (letter.Letter.Equals(name[i]))
+                        {
+                            letter.AddQuantity();
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Word word = new Word(name[i]);
+                    LetterComp.Add(name[i]);
+                    Letters.Add(word);
+                    word.AddQuantity();
+                }
+            }
+            Console.WriteLine("Result:");
+            foreach(Word result in Letters)
+            {
+                Console.WriteLine($"{result.Letter}: {result.Quantity}");
+            }
+        }
+    }
+}
---
+++ Programa para verificar a quantidade de caracteres em uma string/ConsoleApp1/Word.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Word
+    {
+        public char Letter { get; set; }
+        public int Quantity { get; set; }
+        public Word(char Letter)
+        {
+            this.Letter = Letter;
+            Quantity = 0;
+        }
+        public void AddQuantity()
+        {
+            Quantity++;
+        }
+    }
+}
